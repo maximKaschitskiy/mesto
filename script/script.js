2208417b5e6f8d267a092dab07986aa06ee93c1b
Maxim Kaschitskiy <sipus2006@yandex.ru>
2020-12-28 14:43:15 +0300

    исправить ошибки от ревьюэра

--- script/script.js
+++ script/script.js
@@ -3,30 +3,28 @@ let popupForm = document.querySelector('.popup__window');
 let profileContainer = document.querySelector('.profile');
 let showPopupButton = profileContainer.querySelector('.profile__edit-button');
 let closePupupButton = popupContainer.querySelector('.popup__close-icon');
-let submitFormButton = popupContainer.querySelector('.popup__form-submit-button');
 let name = profileContainer.querySelector('.profile__name');
 let status = profileContainer.querySelector('.profile__status');
-let nameField = popupContainer.querySelector('.popup__form-input_name');
-let statusField = popupContainer.querySelector('.popup__form-input_status');
+let nameField = popupContainer.querySelector('.popup__form-input_field_name');
+let statusField = popupContainer.querySelector('.popup__form-input_field_status');
 
 function closePopup() {
     popupContainer.classList.remove('popup_active');
-    popupForm.reset();
   }
 
 function openPopup() {
     popupContainer.classList.add('popup_active');
-    nameField.setAttribute("value", name.textContent);
-    statusField.setAttribute("value", status.textContent);
+    nameField.value = name.textContent;
+    statusField.value = status.textContent;
   }
 
 function submitForm(evt) {
      evt.preventDefault();
-     name.innerHTML = nameField.value;
-     status.innerHTML = statusField.value;
-     popupContainer.classList.remove('popup_active');
+     name.textContent = nameField.value; 
+     status.textContent = statusField.value; 
+     closePopup();
   }
 
 showPopupButton.addEventListener('click', openPopup);
 closePupupButton.addEventListener('click', closePopup);
-submitFormButton.addEventListener('click', submitForm);
+popupForm.addEventListener('submit', submitForm);
