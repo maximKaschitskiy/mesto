cd6ba8f81700f860c6d7dfd7e9f3728aa323c14b
Maxim Kaschitskiy <sipus2006@yandex.ru>
2021-02-04 00:02:58 +0300

    добавить валидацию

--- script/script.js
+++ script/script.js
@@ -24,10 +24,10 @@ const popupFullImageCaption = document.querySelector('.popup__caption');
 const elementContainer = document.querySelector('.elements'); //секция в майн
 const elementTemplate = document.querySelector('#elements-template').content; //темплэйт контейнер
 
-const formAll = document.querySelectorAll('.popup__form-input');//все формы
-
-
-
+const formAll = document.querySelectorAll('.popup__form-input');  //все формы
+//!!!!!!!!!!!!!!!!!!!добавить кнопки сабмита!!!
+const closeEditProfilePupupButton = popupEditProfileContainer.querySelector('.popup__close-button_state_edit-profile'); //кнопка закрыть попап профиль
+const closeAddPlacePupupButton = popupAddPlaceContainer.querySelector('.popup__close-button_state_add-place'); //кнопка закрыть попап место
 
 function resetErrorMessage(errorElement) {
     errorElement.textContent = '';
@@ -48,9 +48,12 @@ function getPopupProfileValues () { //отобразить текущее имя
 
 function submitEditProfileForm(evt) { //отправить форму профиль
      evt.preventDefault();
-     profileNameElement.textContent = nameField.value; 
-     profileStatusElement.textContent = statusField.value;
-     formAll.forEach((input) => { //валидировать!!!
+     function updateEditProfileForm() {
+       profileNameElement.textContent = nameField.value; 
+       profileStatusElement.textContent = statusField.value;
+       closePopup(popupEditProfileContainer);
+     }
+     formAll.forEach((input) => { //валидировать проверить ошибки
        function setErrorMessage(errorElement) {
          switch(input.name) {
            case 'name':
@@ -83,10 +86,11 @@ function submitEditProfileForm(evt) { //отправить форму профи
        resetErrorMessage(errorTextElement);
        if (!isValid(input)) {
          setErrorMessage(errorTextElement);
+     } else {
+         updateEditProfileForm();
        }
-    });
-//     closePopup(popupEditProfileContainer);
-  }
+     });
+   }
 
 function submitAddPlaceForm(evt) { //отправить форму место
      evt.preventDefault();
@@ -101,6 +105,7 @@ function submitAddPlaceForm(evt) { //отправить форму место
 openEditProfilePopupButton.addEventListener('click', function() { //вызов кнопка профиль попап открыть
     openPopup(popupEditProfileContainer);
     getPopupProfileValues();
+    listenFormElements();
   });
 
 closeEditProfilePupupButton.addEventListener('click', function() { // вызов кнопка профиль попап закрыть
@@ -163,5 +168,19 @@ function likePhoto(evt) {
   const eventLike = evt.target;
   eventLike.classList.toggle('elements__like-button_active');
 }
+
+function listenFormElements() { //назначить слушатель на все инпуты
+  formAll.forEach((item) => {
+      item.addEventListener('input', function (evt) {
+        console.log(evt.target.validity.valid);
+    }); 
+  });
+}
+
+function disableButton(button) { //засерить кнопку
+    button.setAttribute('disabled', 'disabled');
+}
+//добавть фунциию засеривания!!!!!!!!!!!!!
+disableButton('')
 
 addDefaultElements();
